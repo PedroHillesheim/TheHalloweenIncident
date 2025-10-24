5d5c86b75893de8d38068747b6eff3a80494872c
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2025-10-24 11:37:47 -0300

    QuitGameAdicionado

--- Assets/Pedro/UiCode.cs
+++ Assets/Pedro/UiCode.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class UiCode : MonoBehaviour
 {
     public void SceneChangeR(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
